bff8674dd25567903a7dbad3515be42f6c79085b
user_name <email_id>
2020-01-23 16:42:00 -0500

    Detector works now

--- CameraController.py
+++ CameraController.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class CameraController:
@@ -16,8 +17,8 @@ class CameraController:
 class Detector:
 
     def __init__(self):
-        self.color_lower_bound = cv2.Scalar(0, 0, 0)  # example value
-        self.color_upper_bound = cv2.Scalar(255, 255, 255)  # example value
+        self.color_lower_bound = np.array([110, 50, 50])  # example value
+        self.color_upper_bound = np.array([130, 255, 255])  # example value
 
     def get_mask_bgr(self, bgr_img):
         # Guassian Blur is used to remove noise from the image
@@ -32,14 +33,14 @@ class Detector:
     def get_rects_from_mask(self, pixel_mask):
         # store a list of contours outlining the shapes leftover
         #       a contour is a list of points outlining a shape in a binary image
-        contours = cv2.findContours(pixel_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(pixel_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # return a list of rectangles that best fit each contour
         return [cv2.minAreaRect(contour) for contour in contours if cv2.contourArea(contour) > 0]
 
     def get_rects_from_bgr(self, bgr_img):
         mask = self.get_mask_bgr(bgr_img)
-        return self.get_rects_from_mask()
+        return self.get_rects_from_mask(mask)
 
 
 def test_camera_to_rect_pipeline():
@@ -48,4 +49,21 @@ def test_camera_to_rect_pipeline():
 
     img = camera.get_image()
     detections = detector.get_rects_from_bgr(img)
+
+
+def test_picture_to_rect_pipeline():
+    detector = Detector()
+    img = cv2.imread("/home/dan/Pictures/oneHandrail.jpg")
+    detections = detector.get_rects_from_bgr(img)
+
+    box = cv2.boxPoints(detections[0])
+    box = np.int0(box)
+    print(box)
+    cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+    cv2.imshow("original", img)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    test_picture_to_rect_pipeline()
 
