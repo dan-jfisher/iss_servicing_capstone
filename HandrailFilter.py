9e9685bdfa4e232d45091c07e9f26cfb1e6ffe40
user_name <email_id>
2020-02-14 13:54:23 -0500

    multiple calibration images

--- HandrailFilter.py
+++ HandrailFilter.py
@@ -1,29 +1,51 @@
 import cv2
 import numpy as np
 from Detector import Detector
+from CameraController import CameraController
+
+
+class CalibrationPackage:
+    def __init__(self, dimension_distance_list=[]):
+        self.dimension_distance_list = dimension_distance_list  # width, height, distance
+
+    def add_detection(self, rotatedRect, dist):
+        dims = rotatedRect[1]
+        width = max(dims)
+        height = min(dims)
+
+        self.dimension_distance_list.append((width, height, dist))
+        return self
 
 
 class HandrailFilter:
 
     def __init__(self):
-        self.known_distance = 100  # cm
         self.known_height = 3.5  # cm
-        self.known_width = 25.5 # cm
+        self.known_width = 25.5  # cm
         self.focal_length_width = -1
         self.focal_length_height = -1
 
-    def calibrate_from_detection(self, rotatedRect):
+    def calibrate_from_package(self, calibration_pckg):
+        # [self.calibrate_from_detection(rect, dist) for rect, dist in calibration_pckg.detection_distances]
+        focal_lengths_list = [(self.get_focal_length_from_detection_width(width, dist),
+                               self.get_focal_length_from_detection_height(height, dist))
+                              for width, height, dist in calibration_pckg.dimension_distance_list]
+
+        self.focal_length_width = sum(pair[0] for pair in focal_lengths_list) / len(focal_lengths_list)
+        self.focal_length_height = sum(pair[1] for pair in focal_lengths_list) / len(focal_lengths_list)
+
+    def calibrate_from_detection(self, rotatedRect, known_distance):
         dims = rotatedRect[1]
         width = max(dims)
         height = min(dims)
-        self.calibrate_from_detection_width(width)
-        self.calibrate_from_detection_height(height)
+        self.focal_length_width = self.get_focal_length_from_detection_width(width, known_distance)
+        self.focal_length_height = self.get_focal_length_from_detection_height(height, known_distance)
 
-    def calibrate_from_detection_height(self, height):
-        self.focal_length_height = height * self.known_distance / self.known_height
+    def get_focal_length_from_detection_height(self, height, known_distance):
+        return height * known_distance / self.known_height
 
-    def calibrate_from_detection_width(self, width):
-        self.focal_length_width = width * self.known_distance / self.known_width
+    def get_focal_length_from_detection_width(self, width, known_distance):
+        return width * known_distance / self.known_width
 
     def get_distance_from_detection(self, rotatedRect):
         dims = rotatedRect[1]
@@ -50,8 +72,8 @@ class HandrailFilter:
         return filtered_rects
 
     def get_valid_detections_and_distances_list(self, rotatedRects):
-        filtered_rects = self.remove_non_handrail_detections(rotatedRects)
-        return [(rect, self.get_distance_from_detection_width(rect)) for rect in filtered_rects]
+        return [(rect, self.get_distance_from_detection(rect))
+                for rect in self.remove_non_handrail_detections(rotatedRects)]
 
 
 def test_distance_calculator():
@@ -59,15 +81,52 @@ def test_distance_calculator():
     handrail_filer = HandrailFilter()
     cal_img = cv2.imread("Calibration_Images/cal_1m_0degrees.jpg")
     detection = detector.get_rects_from_bgr(cal_img)[0]
-    handrail_filer.calibrate_from_detection(detection)
+    handrail_filer.calibrate_from_detection(detection, 100)
 
     test_img = cv2.imread("Calibration_Images/cal_80cm_45degrees.jpg")
     detection = detector.get_rects_from_bgr(test_img)[0]
     print(handrail_filer.get_distance_from_detection(detection))
 
+
 # create a test with multiple handrails in frame
+def test_cam_dist_calculator():
+    detector = Detector()
+    handrail_filer = HandrailFilter()
+    cal_img = cv2.imread("Calibration_Images/cal_1m_0degrees.jpg")
+    detection = detector.get_rects_from_bgr(cal_img)[0]
+    handrail_filer.calibrate_from_detection(detection, 100)
+
+    cam = CameraController()
+    test_img = cam.get_image()
+    detections = detector.get_rects_from_bgr(test_img)
+    out = handrail_filer.get_valid_detections_and_distances_list(detections)
+    for pair in out:
+        box = cv2.boxPoints(pair[0])
+        box = np.int0(box)
+        cv2.drawContours(test_img, [box], 0, (0, 0, 255), 2)
+        img = cv2.resize(test_img, (700, 500))
+        cv2.imshow("original", test_img)
+        cv2.waitKey(0)
+        print(pair[1])
+
+
+def test_package_calibration():
+    detector = Detector()
+    handrail_filer = HandrailFilter()
+    cal_pckg = CalibrationPackage()
+    cal1 = cv2.imread("Calibration_Images/cal_1m_0degrees.jpg")
+    det1 = detector.get_rects_from_bgr(cal1)[0]
+    cal2 = cv2.imread("Calibration_Images/cal_80cm_0degrees.jpg")
+    det2 = detector.get_rects_from_bgr(cal1)[0]
+    cal_pckg.add_detection(det1, 100).add_detection(det2, 80)
+    handrail_filer.calibrate_from_package(cal_pckg)
+
+    test_img = cv2.imread("Calibration_Images/cal_80cm_45degrees.jpg")
+    detection = detector.get_rects_from_bgr(test_img)[0]
+    print(handrail_filer.get_distance_from_detection(detection))
 
 
 if __name__ == "__main__":
-    test_distance_calculator()
-
+    # test_cam_dist_calculator()
+    # test_distance_calculator()
+    test_package_calibration()
